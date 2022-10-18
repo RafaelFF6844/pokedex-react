616fa0edbc57d25a13972e19db6918cd20be6087
Jose Rafael Faña Farias <rafaelfana100@hotmail.com>
2022-10-18 16:01:02 -0400

    Navbar 35%

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import navIcon from '../src/img/pokedex.png';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div className='navbar'>
+        <ul className="navbar-icon-container">
+          <li><img src={navIcon} className="navbar-Icon"></img></li>
+          <li className='navbar-icon-text'>PokeInfo</li>
+        </ul>
+        <ul className="navbar-items-container">
+          <li>Inicio</li>
+          <li>Contacto</li>
+        </ul>
+      </div>
+      <div className='body'>
+
+      </div>
     </div>
   );
 }
