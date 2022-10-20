ac892b9a7af0fc5537b1516d9e4d1498e46751cb
Jose Rafael Faña Farias <rafaelfana100@hotmail.com>
2022-10-20 14:22:17 -0400

    Body UI - En Progreso

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
 import navIcon from '../src/img/pokedex.png';
+import trainerImg from '../src/img/trainer.png';
+import textGlove from '../src/img/globoTexto.png';
+
 
 function App() {
   return (
@@ -12,12 +15,18 @@ function App() {
           <li className='navbar-icon-text'>PokeInfo</li>
         </ul>
         <ul className="navbar-items-container">
-          <li><a href='#'>Inicio</a></li>
+          <li><a href='#'>Servicio</a></li>
           <li><a href='#'>Contacto</a></li>
         </ul>
       </div>
       <div className='body'>
-
+        <div>
+          <img src={trainerImg} className='body-trainerImg'></img>
+          <img src={textGlove} className='body-textGlove'></img>
+        </div>
+        <div className='body-pokesearch'>
+          <input type="text" className='body-pokesearch-bar' id='body-pokesearch-bar'></input>
+        </div>
       </div>
     </div>
   );
